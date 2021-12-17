5d969c1a7ded14d21a72e30d7e85d8b14018bd79
zuohuahua <zuohuahua@elastos.org>
2021-12-17 10:35:53 +0800

    add deploy LockLP script

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,6 +1,13 @@
 const LockToken = artifacts.require("LockToken");
+const LockLP = artifacts.require("LockLP");
 
 module.exports = function (deployer) {
   deployer.deploy(LockToken, 1668823200);
-  //deployer.deploy(LockToken, 1637298000);
+
+  await deployer.deploy(LockLP,
+    LockToken.address,
+    '0xE36FFD17B2661EB57144cEaEf942D95295E637F0', // token0
+    '0x0298c2b32eae4da002a15f36fdf7615bea3da047', // token1
+    '0xED7d5F38C79115ca12fe6C0041abb22F0A06C300'  // router
+    );
 };
